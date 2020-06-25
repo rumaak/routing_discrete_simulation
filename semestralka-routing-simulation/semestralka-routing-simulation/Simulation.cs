6635cf5c2f9741cc48fe3d592e765db0fef8c796
Jan Ruman <janrumik@gmail.com>
2020-06-25 14:50:18 -0700

    WIP continue work on Simulation

--- semestralka-routing-simulation/semestralka-routing-simulation/Simulation.cs
+++ semestralka-routing-simulation/semestralka-routing-simulation/Simulation.cs
@@ -1,36 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Schema;
 
 namespace semestralka_routing_simulation
 {
+    public enum EventType
+    {
+        SendPacket,
+        ProcessPacket,
+        ReceivePacket
+    }
+
+    class Packet
+    {
+        int source, destination;
+        int ID;
+ 
+        public Packet(int source, int destination, int ID)
+        {
+            this.source = source;
+            this.destination = destination;
+            this.ID = ID;
+        }
+    }
+
     abstract class Process
     {
         public int ID;
         public int TimeToProcess;
-        public abstract void HandleEvent(SimulationEvent simEvent);
+
+        public abstract void HandleEvent(SimulationEvent simEvent, Model model);
     }
+    
     class Router : Process
     {
-        public override void HandleEvent(SimulationEvent simEvent)
+        List<Packet> packets;
+        public Router(int id, int timeToProcess)
+        {
+            this.ID = id;
+            this.TimeToProcess = timeToProcess;
+            packets = new List<Packet>();
+        }
+        public void addPacket(Packet packet)
+        {
+            packets.Add(packet);
+        }
+        public override void HandleEvent(SimulationEvent simEvent, Model model)
         {
 
         }
     }
-
+    
     class Computer : Process
     {
-        public override void HandleEvent(SimulationEvent simEvent)
+        List<Packet> packets;
+        public Computer(int id, int timeToProcess)
         {
+            this.ID = id;
+            this.TimeToProcess = timeToProcess;
+            packets = new List<Packet>();
+        }
 
+        public void addPacket(Packet packet)
+        {
+            packets.Add(packet);
+        }
+        public override void HandleEvent(SimulationEvent simEvent, Model model)
+        {
+            if (simEvent.eventType == EventType.SendPacket)
+            {
+                if (packets.Count > 0)
+                {
+                    Packet packet = packets[0];
+                    packets.RemoveAt(0);
+
+                    // TODO 
+
+                    // SimulationEvent simEvent = new SimulationEvent();
+                    // model.scheduler.Add()
+                }
+            }
         }
     }
-
+    
     class Firewall : Process
     {
-        public override void HandleEvent(SimulationEvent simEvent)
+        public Firewall(int id, int timeToProcess)
+        {
+            this.ID = id;
+            this.TimeToProcess = timeToProcess;
+        }
+        public override void HandleEvent(SimulationEvent simEvent, Model model)
         {
 
         }
@@ -40,13 +104,20 @@ namespace semestralka_routing_simulation
     {
         public ulong time;
         public Process process;
+        public EventType eventType;
 
-        public void execute()
+        public SimulationEvent(ulong time, Process process, EventType eventType)
         {
-            process.HandleEvent(this);
+            this.time = time;
+            this.process = process;
+            this.eventType = eventType;
+        }
+
+        public void execute(Model model)
+        {
+            process.HandleEvent(this, model);
         }
     }
-
 
     class Scheduler
     {
@@ -83,33 +154,100 @@ namespace semestralka_routing_simulation
     class Model
     {
         public ulong time;
-        List<Router> routers;
-        List<Computer> computers;
-        List<Firewall> firewalls;
+        public List<Router> routers;
+        public List<Computer> computers;
+        public List<Firewall> firewalls;
+        public Scheduler scheduler;
+        public int[,] routingTable;
 
-        // TODO add parameters with routers, computers, etc
         public Model(Scheduler scheduler)
         {
-            // TODO generate packets from the very start
             time = 0;
+            routers = Simulation.getRouters();
+            computers = Simulation.getComputers();
+            firewalls = Simulation.getFirewalls();
+            routingTable = Simulation.getRouting();
+            this.scheduler = scheduler;
+            Simulation.generatePackets(computers, scheduler, 5, 100);
         }
     }
     
     static class Simulation
     {
+
+        public static List<Router> getRouters()
+        {
+            List<Router> routers = new List<Router>();
+            routers.Add(new Router(1, 1));
+            return routers;
+        }
+
+        public static List<Computer> getComputers()
+        {
+            List<Computer> computers = new List<Computer>();
+            computers.Add(new Computer(2, 1));
+            computers.Add(new Computer(3, 1));
+            return computers;
+        }
+
+        public static List<Firewall> getFirewalls()
+        {
+            List<Firewall> firewalls = new List<Firewall>();
+            firewalls.Add(new Firewall(4, 1));
+            return firewalls;
+        }
+
+        public static int[,] getRouting()
+        {
+            int[,] routing_table = {
+                { 1, 2, 3 },
+                { 2, 2, 1 },
+                { 3, 1, 3 }
+            };
+            return routing_table;
+        }
+ 
+        public static void generatePackets(List<Computer> computers, Scheduler scheduler, int totalPackets, ulong maxTime)
+        {
+            Random rnd = new Random(123);
+
+            int i = 0;
+            while (i < totalPackets)
+            {
+                int from = rnd.Next(computers.Count);
+                int to = rnd.Next(computers.Count);
+                ulong when = Get64BitRandom(maxTime, rnd);
+
+                if (from == to) continue;
+
+                Packet packet = new Packet(computers[from].ID, computers[to].ID, i + 1);
+                computers[from].addPacket(packet);
+                SimulationEvent simEvent = new SimulationEvent(when, computers[from], EventType.SendPacket);
+                scheduler.Add(simEvent);
+
+                Debug.WriteLine($"Created packet with id {i + 1} from computer {computers[from].ID} to computer {computers[to].ID} at time {when}");
+
+                i += 1;
+            }
+        }
+        
+        public static ulong Get64BitRandom(ulong maxValue, Random rnd)
+        {
+            byte[] buffer = new byte[sizeof(ulong)];
+            rnd.NextBytes(buffer);
+            return BitConverter.ToUInt64(buffer, 0) % (maxValue + 1);
+        }
+
         public static void RunSimulation()
         {
             Debug.WriteLine("Simulation started");
             
             Scheduler scheduler = new Scheduler();
-
-            // TODO pass arguments
             Model model = new Model(scheduler);
-            
             SimulationEvent simEvent = scheduler.GetFirst();
             while (simEvent != null)
             {
-                simEvent.execute();
+                simEvent.execute(model);
                 simEvent = scheduler.GetFirst();
             }
         }
