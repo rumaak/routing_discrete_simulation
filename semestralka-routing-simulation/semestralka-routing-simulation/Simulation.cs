959123ad4254f006f1f31041cadcaa3bafad293e
Jan Ruman <janrumik@gmail.com>
2020-06-28 02:45:43 -0700

    Remove unused imports

--- semestralka-routing-simulation/semestralka-routing-simulation/Simulation.cs
+++ semestralka-routing-simulation/semestralka-routing-simulation/Simulation.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Forms;
-using System.Xml.Schema;
 
 namespace semestralka_routing_simulation
 {
